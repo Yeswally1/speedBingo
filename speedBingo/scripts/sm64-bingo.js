5f996e7fbe06d9a305c97739d9eff2b18f285f07
Yeswally1 <35225490+Yeswally1@users.noreply.github.com>
2018-02-24 00:58:55 +0000

    Add files via upload

---
+++ speedBingo/scripts/sm64-bingo.js
@@ -0,0 +1,227 @@
+var bingoList = [];
+bingoList[1] = [
+  { name: "Outdoor Lamps + Lamp Thieves", types: [] },
+  { name: "Outdoor Lamps + Hockey 1", types: [] },
+  { name: "Outdoor Lamps + Bells", types: [] },
+  { name: "Trolley + Lamp Thieves", types: [] },
+  { name: "Trolley + Hockey 1", types: [] },
+  { name: "Trolley + Bells", types: [] },
+  { name: "Lamp Thieves + Hockey 1", types: [] },
+  { name: "Outdoor Lamps + Trolley", types: [] }
+];
+bingoList[2] = [
+  { name: "Cave Lamps + Popcorn 2", types: [] },
+  { name: "Cave Lamps + Cowleks 1", types: [] },
+  { name: "Cave Lamps + Tractor Path", types: [] },
+  { name: "Turtle Boxes + Popcorn 2", types: [] },
+  { name: "Turtle Boxes + Cowleks 1", types: [] },
+  { name: "Turtle Boxes + Tractor Path", types: [] },
+  { name: "Cave Lamps + Turtle Boxes", types: [] }
+];
+bingoList[3] = [
+  { name: "Juliet 1 + George", types: [] },
+  { name: "Juliet 1 + Turtle Soup 2", types: [] },
+  { name: "Juliet 1 + Hippos", types: [] },
+  { name: "Juliet 1 + UFOs 2", types: [] },
+  { name: "Lizard Hunt + George", types: [] },
+  { name: "Lizard Hunt + Turtle Soup 2", types: [] },
+  { name: "Lizard Hunt + UFOs 2", types: [] },
+  { name: "George + Turtle Soup 2", types: [] },
+  { name: "Juliet 1 + Lizard Hunt", types: [] }
+];
+bingoList[4] = [
+  { name: "Juliet 2 + Seahorses", types: [] },
+  { name: "Juliet 2 + Hockey 2", types: [] },
+  { name: "Juliet 2 + Pencil", types: [] },
+  { name: "Floating Mines + Seahorses", types: [] },
+  { name: "Floating Mines + Hockey 2", types: [] },
+  { name: "Floating Mines + Pencil", types: [] },
+  { name: "Juliet 2 + Floating Mines", types: [] }
+];
+bingoList[5] = [
+  { name: "Dinos 2 + Alchemist", types: [] },
+  { name: "Dinos 2 + Mantas 2", types: [] },
+  { name: "Dinos 2 + Basil", types: [] },
+  { name: "Draclets + Alchemist", types: [] },
+  { name: "Draclets + Mantas 2", types: [] },
+  { name: "Draclets + Basil", types: [] },
+  { name: "Alchemist + Mantas 2", types: [] },
+  { name: "Dinos 2 + Draclets", types: [] }
+];
+bingoList[6] = [
+  { name: "Dinos 1 + Mantas 1", types: [] },
+  { name: "Dinos 1 + Bombo", types: [] },
+  { name: "Dinos 1 + Earthshapers", types: [] },
+  { name: "Dinos 1 + UFOs 1", types: [] },
+  { name: "Spirit Statues + Mantas 1", types: [] },
+  { name: "Spirit Statues + Bombo", types: [] },
+  { name: "Spirit Statues + UFOs 1", types: [] },
+  { name: "Mantas 1 + Bombo", types: [] },
+  { name: "Dinos 1 + Spirit Statues", types: [] }
+];
+bingoList[7] = [
+  { name: "Factory Glide 1 + Popcorn 1", types: [] },
+  { name: "Factory Glide 1 + Cowleks 2", types: [] },
+  { name: "Factory Glide 1 + Ox", types: [] },
+  { name: "Turtle Soup 1 + Popcorn 1", types: [] },
+  { name: "Turtle Soup 1 + Cowleks 2", types: [] },
+  { name: "Turtle Soup 1 + Ox", types: [] },
+  { name: "Factory Glide 1 + Turtle Soup 1", types: [] }
+];
+bingoList[8] = [
+  { name: "Factory Glide 2 + Party Crashers", types: [] },
+  { name: "Factory Glide 2 + Puzzle", types: [] },
+  { name: "Factory Glide 2 + Robot Scarecrows", types: [] },
+  { name: "Bone Dance + Party Crashers", types: [] },
+  { name: "Bone Dance + Puzzle", types: [] },
+  { name: "Bone Dance + Robot Scarecrows", types: [] },
+  { name: "Factory Glide 2 + Bone Dance", types: [] }
+];
+bingoList[9] = [
+  { name: "Faun + Monkeys", types: [] },
+  { name: "Faun + Lightning Orbs", types: [] },
+  { name: "Faun + Agent 0", types: [] },
+  { name: "Hula Girls + Monkeys", types: [] },
+  { name: "Hula Girls + Lightning Orbs", types: [] },
+  { name: "Hula Girls + Agent 0", types: [] },
+  { name: "Monkeys + Lightning Orbs", types: [] },
+  { name: "Faun + Hula Girls", types: [] }
+];
+bingoList[10] = [
+  { name: "All gems in Idol Springs", types: [] },
+  { name: "All gems in Hurricos", types: [] },
+  { name: "All gems in Sunny Beach", types: [] },
+  { name: "All gems in Skelos Badlands", types: [] },
+  { name: "All gems in Crystal Glacier", types: [] },
+  { name: "All gems in Scorch", types: [] },
+  { name: "All gems in Fracture Hills", types: [] },
+  { name: "100% Autumn Plains", types: [] },
+  { name: "All gems in Glimmer", types: [] }
+];
+bingoList[11] = [
+  { name: "All orbs in Hurricos", types: [] },
+  { name: "All orbs in Skelos Badlands", types: [] },
+  { name: "All orbs in Crystal Glacier", types: [] },
+  { name: "All orbs in Robotica Farms", types: [] },
+  { name: "100% Magma Cone", types: [] },
+  { name: "100% Colossus", types: [] },
+  { name: "100% Breeze Harbor", types: [] },
+  { name: "100% Summer Forest", types: [] },
+  { name: "All orbs in Glimmer", types: [] }
+];
+bingoList[12] = [
+  { name: "600 combined gems in Glimmer + Shady Oasis", types: [] },
+  { name: "550 combined gems in Glimmer + Mystic Marsh", types: [] },
+  { name: "600 combined gems in Scorch + Shady Oasis", types: [] },
+  { name: "550 combined gems in Scorch + Mystic Marsh", types: [] },
+  { name: "600 combined gems in Glimmer + Scorch", types: [] }
+];
+bingoList[13] = [
+  { name: "600 combined gems in Skelos Badlands + Idol Springs", types: [] },
+  { name: "550 combined gems in Skelos Badlands + Zephyr", types: [] },
+  { name: "550 combined gems in Skelos Badlands + Metropolis ", types: [] },
+  { name: "500 combined gems in Fracture Hills + Zephyr", types: [] },
+  { name: "550 combined gems in Fracture Hills + Idol Springs", types: [] },
+  { name: "500 combined gems in Idol Springs + Zephyr", types: [] },
+  { name: "500 combined gems in Fracture Hills + Metropolis", types: [] },
+  { name: "600 combined gems in Skelos Badlands + Fracture Hills", types: [] }
+];
+bingoList[14] = [
+  { name: "600 combined gems in Breeze Harbor + Magma Cone", types: [] },
+  { name: "550 combined gems in Breeze Harbor + Colossus", types: [] },
+  { name: "550 combined gems in Breeze Harbor + Robotica Farms", types: [] },
+  { name: "600 combined gems in Sunny Beach + Magma Cone", types: [] },
+  { name: "550 combined gems in Sunny Beach + Colossus", types: [] },
+  { name: "500 combined gems in Sunny Beach + Robotica Farms", types: [] },
+  { name: "500 combined gems in Magma Cone + Colossus", types: [] },
+  { name: "600 combined gems in Breeze Harbor + Sunny Beach", types: [] }
+];
+bingoList[15] = [
+  { name: "500 combined gems in Hurricos + Aquaria Towers", types: [] },
+  { name: "500 combined gems in Hurricos + Cloud Temples", types: [] },
+  { name: "500 combined gems in Crystal Glacier + Aquaria Towers", types: [] },
+  { name: "500 combined gems in Crystal Glacier + Cloud Temples", types: [] },
+  { name: "600 combined gems in Hurricos + Crystal Glacier", types: [] }
+];
+bingoList[16] = [
+  { name: "4 combined orbs in Skelos Badlands + Scorch", types: [] },
+  { name: "3 combined orbs in Idol Springs + Breeze Harbor", types: [] },
+  { name: "4 combined orbs in Colossus + Aquaria Towers", types: [] },
+  { name: "4 combined orbs in Magma Cone + Fracture Hills", types: [] },
+  { name: "5 combined orbs in Hurricos + Zephyr", types: [] },
+  { name: "3 combined orbs in Crystal Glacier + Shady Oasis", types: [] },
+  { name: "4 combined orbs in Glimmer + Sunny Beach", types: [] }
+];
+bingoList[17] = [
+  { name: "250 gems in both Idol Springs + Crystal Glacier", types: [] },
+  { name: "300 gems in both Glimmer + Skelos Badlands", types: [] },
+  { name: "300 gems in both Idol Springs + Breeze Harbor", types: [] },
+  { name: "275 gems in both Glimmer + Metropolis", types: [] },
+  { name: "250 gems in both Glimmer + Magma Cone", types: [] }
+];
+bingoList[18] = [
+  { name: "250 gems in both Hurricos + Scorch", types: [] },
+  { name: "225 gems in both Colossus + Zephyr", types: [] },
+  { name: "275 gems in both Hurricos + Magma Cone", types: [] },
+  { name: "200 gems in both Mystic Marsh + Robotica Farms", types: [] },
+  { name: "250 gems in both Colossus + Breeze Harbor", types: [] }
+];
+bingoList[19] = [
+  { name: "250 gems in both Aquaria Towers + Skelos Badlands", types: [] },
+  { name: "275 gems in both Sunny Beach + Scorch ", types: [] },
+  { name: "250 gems in both Aquaria Towers + Fracture Hills", types: [] },
+  { name: "250 gems in both Sunny Beach + Cloud Temples", types: [] },
+  { name: "250 gems in both Sunny Beach + Shady Oasis", types: [] }
+];
+bingoList[20] = [
+  { name: "Idol Springs, Skelos Badlands & Scorch Talismans", types: [] },
+  { name: "Sunny Beach, Zephyr & Crystal Glacier Talismans", types: [] },
+  { name: "Magma Cone, Breeze Harbor & Shady Oasis Talismans", types: [] },
+  { name: "2 End of Level Orbs", types: [] },
+  { name: "All gems in 2 speedways", types: [] },
+  { name: "2 Speedway Orbs", types: [] },
+  { name: "Colossus, Hurricos & Fracture Hills Talismans", types: [] }
+];
+bingoList[21] = [
+  { name: "1000 gems in Summer Forest", types: [] },
+  { name: "1500 gems in Autumn Plains", types: [] },
+  { name: "12 orbs in Summer Forest", types: [] },
+  { name: "12 orbs in Autumn Plains", types: [] },
+  { name: "6 orbs in Winter Tundra", types: [] },
+  { name: "2000 gems", types: [] }
+];
+bingoList[22] = [
+  { name: "Use all '14' powerups", types: [] },
+  { name: "Use all '18' powerups", types: [] },
+  { name: "Use 4 different powerup types", types: [] },
+  { name: "Use all '15' powerups", types: [] },
+  { name: "Collect all Spirit Particles in 3 levels", types: [] },
+  { name: "Use all instances of the same powerup type", types: [] },
+  { name: "Use all '2' powerups", types: [] }
+];
+bingoList[23] = [
+  { name: "Catbat Quartet + Under 1:10 in Ocean Speedway' Skill Points", types: [] },
+  { name: "Land on Idol + Windmills' Skill Points", types: [] },
+  { name: "Seaweed' Skill Point", types: [] },
+  { name: "5 Skill Points", types: [] },
+  { name: "Cacti + 3 Laps' Skill Points", types: [] },
+  { name: "Coconut Trees + Under 1:15 in Icy Speedway' Skill Points", types: [] },
+  { name: "3 Boss Skill Points", types: [] }
+];
+bingoList[24] = [
+  { name: "Enter 5 exit level portals", types: [] },
+  { name: "6 Pink Gems", types: [] },
+  { name: "Pay Moneybags 5 times", types: [] },
+  { name: "6 Hunter Orbs", types: [] },
+  { name: "Buy 2 abilites from Moneybags", types: [] },
+  { name: "Stand on 10 Portals", types: [] }
+];
+bingoList[25] = [
+  { name: "Break 12 Firework Bottles", types: [] },
+  { name: "Break 15 Butterfly Jars", types: [] },
+  { name: "Break 3 Mystery Jars", types: [] },
+  { name: "Fire 4 Rockets", types: [] },
+  { name: "Break 9 Strong Chests", types: [] }
+];
+
+$(function() { srl.bingo(bingoList, 5); });
